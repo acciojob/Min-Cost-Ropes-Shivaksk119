4d1833152c4dcb4a3b95bfce38bf94b573fb3301
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-23 15:48:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+
+	arr.sort();
+	let minCost = 0;
+	let lengthOfRope = arr[0];
+	for(let i=1;i<arr.length;i++){
+		lengthOfRope = lengthOfRope+arr[i];
+		minCost = minCost + lengthOfRope;
+	}
+	retrun currCost;
   
 }
 
