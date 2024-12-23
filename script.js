c3439f047f676de1b280ad58b468bfe302cd4708
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-23 19:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
 	arr.sort();
 	let cost = 0;
-	let l1 = parseInt(arr.length/2);
-	
-	let lor1 = arr[0];
-	
-	for(let i=1;i<=l1;i++){
-	  lor1 = lor1 + arr[i];
-	   cost += lor1;
+	while(arr.length>1){
+	  let n1 = arr.shift();
+	  let n2 = arr.shift();
+	  let sum = n1+n2;
+	  cost += sum;
+	  console.log(n1,n2,sum,cost)
+	  arr.push(sum);
+	  insertAtOrder(sum,arr);
 	}
-	
-	let lor2 = arr[arr.length-1];
-	
-	for(let j=arr.length-2;j>l1;j--){
-	    lor2 = lor2 + arr[j];
-	    cost += lor2;
-	}
-	
-	cost = cost+lor1+lor2;
-  return cost;
+	return cost;
 }
+
+const insertAtOrder=(num,arr)=>{
+  let i=0;
+  while(i<arr.length && arr[i]<num){
+    i++;
+  }
+  return arr.splice(i,0,num)
+}
+
 
 module.exports=mincost;
